d2a044fd381522320f0198a571d6d4c836780fea
naraharidhivi <111236432+naraharidhivi@users.noreply.github.com>
2022-08-14 13:55:13 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,10 @@
-
-
-function checkButton() {  
-    if(document.getElementById('option_one').checked) { 
-        document.getElementById("reveal-if-active").innerHTML 
-            = ""  
-    } 
-    else if(document.getElementById('option_two').checked) { 
-        document.getElementById("reveal-if-active").innerHTML 
-            = ""
-    } 
-    else if(document.getElementById('option_three').checked) { 
-            document.getElementById("reveal-if-active").innerHTML 
-            = "<br> <label for='hourlyRate'>Hourly Rate</label><input type='text' id='hourlyRate' name='hourlyRate' placeholder='Hourly Rate here...' required pattern='[0-9]+'/>"
-    } 
-} 
+function checkButton() {
+  if (document.getElementById("option1").checked) {
+    document.getElementById("reveal-if-active").innerHTML = "";
+  } else if (document.getElementById("option2").checked) {
+    document.getElementById("reveal-if-active").innerHTML = "";
+  } else if (document.getElementById("option3").checked) {
+    document.getElementById("reveal-if-active").innerHTML =
+      "<br> <label for='hourlyRate'>Hourly Rate</label><input type='text' id='hourlyRate' name='hourlyRate' placeholder='Hourly Rate here...' required pattern='[0-9]+'/>";
+  }
+}
